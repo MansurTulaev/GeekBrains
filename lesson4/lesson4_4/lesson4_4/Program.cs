4ded4baa17c85bc8859b52c33ff1b48d4f55e225
MansurTulaev <manstul@mail.ru>
2022-02-03 19:07:23 +0300

    Lesson4. All tasks

---
+++ lesson4/lesson4_4/lesson4_4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace lesson4_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число для подсчета числа Фибоначчи:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(GetFibonacci(num));
+            for (int i = 0; i <= num; i++)
+                Console.WriteLine($"Для {i} число Фибоначчи: {GetFibonacci(i)}");
+        }
+        static int GetFibonacci(int n)
+        {
+            if (n == 0)
+                return 0;
+            else if (n == 1)
+                return 1;
+            else
+                return GetFibonacci(n - 1) + GetFibonacci(n - 2);
+        }
+    }
+}

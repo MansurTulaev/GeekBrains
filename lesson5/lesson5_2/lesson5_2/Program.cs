54d18064f4ef3ebde188d819064b41d845560090
MansurTulaev <manstul@mail.ru>
2022-02-14 18:43:23 +0300

    Lesson 5. Tasks 1-5

---
+++ lesson5/lesson5_2/lesson5_2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace lesson5_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Идет запись в текстовый файл");
+            File.AppendAllText("startup.txt", Environment.NewLine+DateTime.Now.ToString());
+        }
+    }
+}
---
+++ lesson5/lesson5_4/lesson5_4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace lesson5_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите путь к файлу");
+            string fileName = Console.ReadLine();
+            static void GetDirectoriesRecursive(string path, string file)
+            {
+                File.AppendAllLines(file, new[] { path });
+                var subdirectories = Directory.EnumerateDirectories(path);
+                foreach (string subdirectory in subdirectories)
+                    GetDirectoriesRecursive(subdirectory, file);
+            }
+            GetDirectoriesRecursive(fileName, "ololo.txt");
+
+
+        }
+    }
+}

cb965ffde6e95502acbd342402e07435212eea50
MansurTulaev <manstul@mail.ru>
2022-02-24 11:49:57 +0300

    Lesson 8. Project Settings

---
+++ lesson8/ConsoleApp6/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("ConsoleApp6")]
+[assembly: AssemblyDescription("Урок 7. Версия сборки 1.001 и версия файла 1.003")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("MansDevelopment")]
+[assembly: AssemblyProduct("ProjectSettings")]
+[assembly: AssemblyCopyright("Copyright ©  MansDevelopment 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("f08326af-2c34-4bc7-a4d4-2f4fb58066af")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.3")]

8166d4ae49b5194fb579e5ca0e1e73283bfe8251
MansurTulaev <manstul@mail.ru>
2022-04-03 22:19:21 +0300

    Lesson 3. Transfer Money and Reversed String

---
+++ OOP/lesson3/lesson3.1/BankAccount/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+   //создание перечисления тип аккаунтов
+   enum AccountTypes
+    {
+        Correspondent,
+        Personal,
+        Retirement
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount firstBankAccount = new BankAccount(300000, (int)AccountTypes.Correspondent);
+            BankAccount secondBankAccount = new BankAccount(400000, (int)AccountTypes.Correspondent);
+            firstBankAccount.TransferMoney(100000, secondBankAccount);
+            Console.ReadLine();
+
+        }
+    }
+
+    class BankAccount
+    {
+        private int _balance;
+        private int _accountType;
+        public static int _generatedId; //статическое поле
+        private int _accountId;
+
+        public int AccountId { get; set; }
+        public BankAccount()
+        {
+            GenerateId();
+            this.AccountId = _generatedId;
+        }
+        public BankAccount(int newBalance, int newType)
+        {
+            this.Type = newType;
+            this.Balance = newBalance;
+            GenerateId();
+            this.AccountId = _generatedId;
+        }
+         public static void GenerateId()
+        {
+            ++_generatedId;
+        }
+        public int Balance { get; set; }
+        public int Type { get; set; }
+        public void TransferMoney(int sum, BankAccount otherBankAccount)
+        {
+            if (this.Balance<sum)
+            {
+                Console.WriteLine("Transfer is not possible. You do not have enough money");
+            }
+            otherBankAccount.Balance += sum;
+            this.Balance -= sum;
+            Console.WriteLine($"Current ID {this.AccountId}. Balance: {this.Balance}. You transferred {sum} to other account");
+            Console.WriteLine($"Other Account ID {otherBankAccount.AccountId}. Balance: {otherBankAccount.Balance}");
+        }
+    }
+}
---
+++ OOP/lesson3/lesson3.2/lesson3.2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string firstString = "Mans";
+            Console.WriteLine("Enter the word");
+            string firstString = Console.ReadLine();
+            Console.WriteLine($"Input String is {firstString}. Reversed String is {ReverseString(firstString)}");
+            Console.ReadKey();
+        }
+        public static string ReverseString(string input)
+        {
+            char[] stringToChar = input.ToCharArray();
+            Array.Reverse(stringToChar);
+            return new string (stringToChar);
+        }
+    }
+}
